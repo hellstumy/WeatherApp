52c8910a29634455e151109f187045e7c22fc7a2
hellstumy <84933162+hellstumy@users.noreply.github.com>
2025-09-09 21:48:53 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,9 @@
     }
   };
     
-    if (!weather) return <p>Загрузка...</p>;
+    if (!weather) return <div className="loading_conteiner">
+      <div className="loading"><span>L</span><span>o</span><span>a</span><span>d</span><span>i</span><span>n</span><span>g</span></div>
+    </div>;
 
     let temp = weather.main.temp
     temp = Math.round(temp)
